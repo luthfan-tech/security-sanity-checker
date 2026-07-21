69bc906d240d6b7bc720bb0e6155a1360a0a884b
Lutfan <luthfanmak@gmail.com>
2026-07-21 20:05:41 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+from .passwords import check_password_strength
+from .secrets import scan_file_for_secrets
+from .urls import check_url_risk
+from .logs import parse_failed_logins
+
+__all__ = [
+    "check_password_strength",
+    "scan_file_for_secrets",
+    "check_url_risk",
+    "parse_failed_logins",
+]
+
 On Linux/macOS:
 Bash
 touch checks/__init__.py
@@ -5,9 +17,3 @@ touch checks/__init__.py
 On Windows (PowerShell):
 PowerShell
 New-Item -Path checks/__init__.py -ItemType File
-
-
-from .passwords import check_password_strength
-from .secrets import scan_file_for_secrets
-from .urls import check_url_risk
-from .logs import parse_failed_logins
